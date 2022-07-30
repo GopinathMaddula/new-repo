c8033f04c4c0878b506f2682f11268b080094c7b
GopinathMaddula <101272335+GopinathMaddula@users.noreply.github.com>
2022-07-30 13:56:27 +0530

    removed img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Rem <code>src/App.js</code> and save to reload.
-        </p>
+        
+        
         <a
           className="App-link"
           href="https://reactjs.org"
